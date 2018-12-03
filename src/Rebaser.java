092a79a0f612bf5b0baa61a2f6a2ac2c88031d06
GeneralLol <dsqchn@gmail.com>
2018-12-03 16:52:06 -0500

    Skeleton finished

--- src/Rebaser.java
+++ src/Rebaser.java
@@ -2,5 +2,36 @@
  * Rebaser class. 
  */
 public class Rebaser {
+	String numStr; 
 	
+	/**
+	 * Default Constructor. 
+	 * 
+	 */
+	public Rebaser() {
+		
+	}
+	/**
+	 * Constructor. 
+	 * @param num Input value for number desired. String. 
+	 */
+	public Rebaser(String num) {
+		
+	}
+	
+	public void setValue(String val) {
+		
+	}
+	
+	public String getValue() {
+		
+	}
+	
+	public String convertToBaseN(int base) {
+		
+	}
+	
+	public String convertToBaseTen(int base) {
+		
+	}
 }
--- src/Tester.java
+++ src/Tester.java
@@ -5,7 +5,6 @@ public class Tester {
 
 	public static void main(String[] args) {
 
-
 	}
 
 }
