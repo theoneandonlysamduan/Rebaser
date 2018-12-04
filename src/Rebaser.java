da96c34966bffa4429888ff557c0146d0aa445b6
GeneralLol <dsqchn@gmail.com>
2018-12-03 20:11:28 -0500

    previous commit wrong branch

--- src/Rebaser.java
+++ src/Rebaser.java
@@ -2,5 +2,50 @@
  * Rebaser class. 
  */
 public class Rebaser {
+	String numStr; 
 	
+	/**
+	 * Default Constructor. 
+	 * 
+	 */
+	public Rebaser() {
+		
+	}
+	/**
+	 * Constructor. 
+	 * @param num Input value for number desired. String. 
+	 */
+	public Rebaser(String num) {
+		numStr = num; 
+	}
+	
+	/**
+	 * Sets the value of the stored value. 
+	 * @param val Input value. 
+	 */
+	public void setValue(String val) {
+		numStr = val;
+	}
+	
+	/**
+	 * Returns the stored value. 
+	 * @return stored value of the object. 
+	 */
+	public String getValue() {
+		return numStr; 
+	}
+	
+	/**
+	 * Converts the stored value(treated as base 10) into any given base. 
+	 * Does not change the stored value. 
+	 * @param base the desired base to be converted into. 
+	 * @return stored value converted into desired base. 
+	 */
+	public String convertToBaseN(int base) {
+		
+	}
+	
+	public String convertToBaseTen(int base) {
+		
+	}
 }
