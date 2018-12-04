c4e674a6ad6880c4492676034782e6f9820cf5c9
GeneralLol <dsqchn@gmail.com>
2018-12-03 20:36:58 -0500

    fleshed out ifLegal()

--- src/Rebaser.java
+++ src/Rebaser.java
@@ -3,13 +3,17 @@
  */
 public class Rebaser {
 	String numStr; 
+	char[] numCharArr;
+	
+	static final int STORED_BASE = 10; 
 	
 	/**
 	 * Default Constructor. 
 	 * 
 	 */
 	public Rebaser() {
-		
+		numStr = "";
+		numCharArr = numStr.toCharArray();
 	}
 	/**
 	 * Constructor. 
@@ -17,6 +21,7 @@ public class Rebaser {
 	 */
 	public Rebaser(String num) {
 		numStr = num; 
+		numCharArr = numStr.toCharArray();
 	}
 	
 	/**
@@ -42,10 +47,66 @@ public class Rebaser {
 	 * @return stored value converted into desired base. 
 	 */
 	public String convertToBaseN(int base) {
+		//Check if the stored value is legal. 
+		if (ifLegal(STORED_BASE)) {
+			return "Error";
+		}
+		return "dummy";
+		
 		
 	}
 	
 	public String convertToBaseTen(int base) {
+		return "dummy";
+	}
+	
+	/**
+	 * Private Implimentation that checks whether the stored
+	 * number is legal as an input. 
+	 * @param base base which the number is of. 
+	 * @return true or false depending on whether it is a 
+	 * 		   legal number or not. 
+	 */
+	private boolean ifLegal(int base) {
+		//TODO: Currently not working. Returns true when not 
+		// supposed to. 
+		boolean rst = true; 
 		
+		for (char i: numCharArr) {
+			
+			//If there is a letter and the base is less than
+			// 11, automatically fail. 
+			if(Character.isLetter(i) && base <= 10) {
+				rst = false; 
+				break; 
+			}
+			
+			//Check to see if the current char is a number. 
+			if (Character.isDigit(i)) {
+				
+				//If the char is a number, check to see if
+				// it is larger than base - 1. 
+				// If so set rst to false,
+				// end everything. 
+				if ((i - 30) > (base - 1)) {
+					rst = false;
+					break; 
+				}
+				
+			}else if(Character.isLetter(i)) {
+				
+				//If the char is a letter, check to see if 
+				// it exceeds the maximum allowance of the 
+				// base (base - 1 + 55 to ascii). 
+				// If so set rst to false, end everything. 
+				if (i > (base - 1 + 55)) {
+					rst = false;
+					break; 
+				}
+			}
+		}
+		
+		//If the program ended no problem, rst is true by default. 
+		return rst; 
 	}
 }
--- src/Tester.java
+++ src/Tester.java
@@ -4,7 +4,10 @@
 public class Tester {
 
 	public static void main(String[] args) {
-
+		Rebaser rebaser = new Rebaser("12A");
+		String  foo = rebaser.convertToBaseN(2);
+		
+		System.out.println(foo);
 
 	}
 
