66179ec02d5e43ae3cc1cbe1b0414fc0f2605d14
Haijian <19bwang@derryfield.org>
2018-12-12 10:08:44 -0500

    Updates

--- src/Tester.java
+++ src/Tester.java
@@ -7,7 +7,7 @@ public class Tester {
 		Rebaser c = new Rebaser("567475"); // initialize a base 8 number
 		Rebaser d = new Rebaser("1A9247"); // initialize a base 11 number
 		Rebaser e = new Rebaser("ABCDCD");// initialize a base 14 number
-		
+		Rebaser f = new Rebaser("cca");//innitialize a base 14 number
 
 		System.out.println(e.convertToBaseTen(14));
 		System.out.println("Expected: 5836473");
@@ -22,7 +22,17 @@ public class Tester {
 		System.out.println("Expected: 482");
 		
 		System.out.println(b.convertToBaseN(12));
+		System.out.println("Expected: 5A884");
+		
 		System.out.println(d.convertToBaseN(12));
+		System.out.println("Expected: -1");
+		
+		System.out.println(f.convertToBaseTen(14));
+		System.out.println("Expected: 2530");
+		System.out.println(f.getValue());
+		System.out.println("Expected: CCA");
+		
+		
 		
 	}
 
